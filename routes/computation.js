826149507481f1992f9175afd026ee833fa4b4a9
AvinashChinnala1308 <avinashchinnala1325@gmail.com>
2023-10-21 21:27:53 -0500

    computed values

--- routes/computation.js
+++ routes/computation.js
@@ -4,10 +4,18 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    const randomValue = (Math.random() * 2) - 1;
-    const result = Math.asin(randomValue);
+    const randomValue = Math.floor(Math.random() * (20)) + 1;
 
-    res.send(`Math.asin applied to ${randomValue} is ${result}`);
+const res2 = Math.asinh(randomValue);
+const res3 = Math.cos(randomValue);
+
+const response = 
+                `Math.asinh applied to ${randomValue} is ${res2}\n` + `        and    `+
+                `Math.cos applied to ${randomValue} is ${res3}\n` ;
+
+res.send(response);
+
+    
 });
 
 
