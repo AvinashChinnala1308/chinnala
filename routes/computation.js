8bb27bda0d43f1d05f0a750f5a5611ddc8fc6e2e
AvinashChinnala1308 <avinashchinnala1325@gmail.com>
2023-10-25 22:47:08 -0500

    updated computation

--- routes/computation.js
+++ routes/computation.js
@@ -4,7 +4,14 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    const randomValue = Math.floor(Math.random() * (20)) + 1;
+    var c = req.query.c;
+    var randomValue;
+    if(c != undefined){
+        randomValue =c;
+    }
+    else{
+        randomValue = Math.random();
+    }
 
 const res2 = Math.asinh(randomValue);
 const res3 = Math.cos(randomValue);
@@ -12,8 +19,9 @@ const res4 = Math.sin(randomValue);
 
 
 const response = 
-                `Math.asinh applied to ${randomValue} is ${res2}\n` + `        and    `+
-                `Math.cos applied to ${randomValue} is ${res3}\n`  +`        and    `+
+                `Math.asinh applied to ${randomValue} is ${res2}\n` + `        <br>    `+
+                
+                `Math.cos applied to ${randomValue} is ${res3}\n`  +`      <br>      `+
                 `Math.sin  applied to ${randomValue} is ${res4}\n`;
 
 res.send(response);
